f8908c31895de6444c5f76a50e461d578dfb24bd
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-02-18 14:21:33 +0200

    Initial Commit

---
+++ ColorSliderPickerSwiftUI/ColorSliderPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderPickerSwiftUIApp.swift
+//  ColorSliderPickerSwiftUI
+//
+//  Created by Кирилл Тараско on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
